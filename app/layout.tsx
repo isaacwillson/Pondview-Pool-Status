dc6f793be2e0bfe1230864a4e06aec300a84d011
Isaac Willson <isaac.willson2007@gmail.com>
2026-06-27 19:28:23 -0400

    removed redundant eyebrows, and added vercel analytic link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Instrument_Serif } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const inter = Inter({
